1b575aba132222d28de04dc765e68c66d48cd3f2
smcguire56 <sean.mcguire56@gmail.com>
2018-12-10 22:53:35 +0000

    update

--- CarHireProject/RMIClient/src/ie/gmit/sw/Model/Car.java
+++ CarHireProject/RMIClient/src/ie/gmit/sw/Model/Car.java
@@ -17,7 +17,6 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlSchemaType;
 import javax.xml.bind.annotation.XmlType;
-import javax.xml.datatype.XMLGregorianCalendar;
 
 
 /**
